18bf5d792ce79476a6df1ab1ee67ad5e79b656d0
HaniBk96 <161308224+HaniBk96@users.noreply.github.com>
2024-03-12 23:11:26 +0200

    add class

--- profilio/javascript/main.js
+++ profilio/javascript/main.js
@@ -4,9 +4,9 @@ let lio = document.getElementById("lio");
 
 window.onscroll = function () {
 if (window.scrollY >= 50) {
-    header.style.backgroundColor = "red";
-    lio.style.Color = "white";
+    header.classList.add("hanibknav");
+
 } else {
-    header.style.backgroundColor = "#ffffff00";
+    header.classList.remove("hanibknav");
 }
 }
