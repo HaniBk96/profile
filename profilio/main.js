7a93fb6625ed6defcb6da403eb6a8b23d8bd025e
HaniBk96 <161308224+HaniBk96@users.noreply.github.com>
2024-03-15 13:37:29 +0200

    a

--- profilio/main.js
+++ profilio/main.js
@@ -3,10 +3,22 @@ let lio = document.getElementById("lio");
 
 
 window.onscroll = function () {
-if (window.scrollY >= 50) {
-    header.classList.add("hanibknav");
+    if (window.scrollY >= 50) {
+        header.classList.add("hanibknav");
+        
+    } else {
+        header.classList.remove("hanibknav");
+    }
+}
 
-} else {
-    header.classList.remove("hanibknav");
-}
-}
+let menuul = document.getElementById("menu-ul");
+let fontmen = document.getElementById("font-menu");
+
+fontmen.onclick = function() {
+    menuul.classList.add("active");
+    fontmen.classList.add("active");
+    this.onclick = function() {
+        menuul.classList.remove("active");
+        fontmen.classList.remove("active");
+    }
+};
