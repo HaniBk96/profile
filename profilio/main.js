cc71340919ea39a002ba69864400b55aabce41c7
HaniBk96 <161308224+HaniBk96@users.noreply.github.com>
2024-03-15 17:33:00 +0200

    1

--- profilio/main.js
+++ profilio/main.js
@@ -1,3 +1,6 @@
+
+    //    chanig colo nav bavbar when scrole 
+
 let header = document.getElementById("main-header");
 let lio = document.getElementById("lio");
 
@@ -11,14 +14,30 @@ window.onscroll = function () {
     }
 }
 
+
+            //   add menu btn  
 let menuul = document.getElementById("menu-ul");
 let fontmen = document.getElementById("font-menu");
+let i =document.querySelector("#font-menu i")
 
 fontmen.onclick = function() {
     menuul.classList.add("active");
     fontmen.classList.add("active");
+    i.classList.add("active");
     this.onclick = function() {
         menuul.classList.remove("active");
         fontmen.classList.remove("active");
     }
 };
+
+        //   add typing short 
+
+function type() {
+    var typed = new Typed(".typing", {
+        strings:["Freelancer","Developer","Youtuber","Bloger"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true,
+    })
+};
+type();
